b7debd7facf9af5783d1a38f8694c1d965b9baea
Simon Kamangaru <63295027+SimonMunyiri@users.noreply.github.com>
2026-04-13 23:43:03 +0300

    Implement auth on sign up page

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { signUpSchema } from "@/app/schema/auth";
 import { Button } from "@/components/ui/button";
@@ -8,10 +8,17 @@ import {
   CardDescription,
   CardHeader,
 } from "@/components/ui/card";
-import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
+import z from "zod";
 
 export default function SignUpPage() {
   // Initialize the form with react-hook-form and zod validation
@@ -24,8 +31,12 @@ export default function SignUpPage() {
     },
   });
 
-  function onSubmit() {
-    console.log('Done')
+  async function onSubmit(data: z.infer<typeof signUpSchema>) {
+    await authClient.signUp.email({
+      email: data.email,
+      name: data.name,
+      password: data.password,
+    });
   }
 
   return (
@@ -39,14 +50,17 @@ export default function SignUpPage() {
               <Controller
                 name="name"
                 control={form.control}
-                render={({field, fieldState}) => (
+                render={({ field, fieldState }) => (
                   <Field>
                     <FieldLabel>Full Name</FieldLabel>
-                    <Input aria-invalid={fieldState.invalid} placeholder="John Doe" {...field} /> 
+                    <Input
+                      aria-invalid={fieldState.invalid}
+                      placeholder="John Doe"
+                      {...field}
+                    />
                     {fieldState.error && (
-                        <FieldError errors={[fieldState.error]} />
-                    )
-                }
+                      <FieldError errors={[fieldState.error]} />
+                    )}
                   </Field>
                 )}
               />
@@ -54,14 +68,17 @@ export default function SignUpPage() {
               <Controller
                 name="email"
                 control={form.control}
-                render={({field, fieldState}) => (
+                render={({ field, fieldState }) => (
                   <Field>
                     <FieldLabel>Email</FieldLabel>
-                    <Input aria-invalid={fieldState.invalid}placeholder="john@example.com" {...field} /> 
+                    <Input
+                      aria-invalid={fieldState.invalid}
+                      placeholder="john@example.com"
+                      {...field}
+                    />
                     {fieldState.error && (
-                        <FieldError errors={[fieldState.error]} />
-                    )
-                }
+                      <FieldError errors={[fieldState.error]} />
+                    )}
                   </Field>
                 )}
               />
@@ -69,14 +86,18 @@ export default function SignUpPage() {
               <Controller
                 name="password"
                 control={form.control}
-                render={({field, fieldState}) => (
+                render={({ field, fieldState }) => (
                   <Field>
                     <FieldLabel>Password</FieldLabel>
-                    <Input aria-invalid={fieldState.invalid} placeholder="••••••••" type="password" {...field} /> 
+                    <Input
+                      aria-invalid={fieldState.invalid}
+                      placeholder="••••••••"
+                      type="password"
+                      {...field}
+                    />
                     {fieldState.error && (
-                        <FieldError errors={[fieldState.error]} />
-                    )
-                }
+                      <FieldError errors={[fieldState.error]} />
+                    )}
                   </Field>
                 )}
               />
